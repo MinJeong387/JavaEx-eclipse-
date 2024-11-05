f9d7bfba8f03eda0979f4b4a12aaca6df87aedd7
서민정 <ybsm0629@gmail.com>
2024-11-05 23:22:26 +0900

    연습중

--- src/com/javaex/practice03/Ex08.java
+++ src/com/javaex/practice03/Ex08.java
@@ -10,6 +10,18 @@ public class Ex08 {
 	
 	private static void ex08() {
 		
+		int i;
+		int j;
+		
+		for (i=2 ; i<10 ; ++i) {
+			for(j=1;j<10;++j) {
+				
+				System.out.println(i*j);
+				
+				
+			}
+		}
+		
 		
 		
 		
