94c67d2cd3e0a6a31a3c6cbe9a429704df944914
서민정 <ybsm0629@gmail.com>
2024-11-10 14:49:10 +0900

    완료

--- src/com/javaex/oop/shape/v2/ShapeApp.java
+++ src/com/javaex/oop/shape/v2/ShapeApp.java
@@ -15,13 +15,14 @@ public class ShapeApp {
 		Point p = new Point(100, 100);
 		p.draw();
 
+		
 		Drawable[] objs = new Drawable[] { r, c, p };
 
 		// 특정 객체가 특정 인터페이스를 구현한 객체인가를 판단하려면 (instanceof 의 두번째 기능)
 		if (r instanceof Drawable) {
 			System.out.println("r은 Drawable의 구현체입니다.");
 		}
-
+		System.out.println();
 		for (Drawable obj : objs) {
 			obj.draw();
 		}
