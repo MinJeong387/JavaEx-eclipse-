e888b50526b87fd4e44774800642284be0cdc092
MinJeong387 <ybsm0629@gmail.com>
2024-11-11 16:23:46 +0900

    연습

---
+++ src/com/javaex/api/arrays/ArraysSortEx.java
@@ -0,0 +1,15 @@
+package com.javaex.api.arrays;
+
+public class ArraysSortEx {
+
+	public static void main(String[] args) {
+		basicSortTest(); // 기본 타입 소트
+
+	}
+
+	private static void basicSortTest() {
+
+		int[] scores = { 80, 50, 30, 90, 75, 88, 77, };
+	}
+
+}
