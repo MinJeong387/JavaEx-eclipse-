1c6945dc0ade85aa6262d930a9573ab2538ec3f7
MinJeong387 <ybsm0629@gmail.com>
2024-11-25 16:53:43 +0900

    저자 정보 삭제 구현

--- src/com/javaex/jdbc/dao/AuthorDaoImpl.java
+++ src/com/javaex/jdbc/dao/AuthorDaoImpl.java
@@ -11,51 +11,52 @@ import java.util.Date;
 import java.util.List;
 
 public class AuthorDaoImpl implements AuthorDao {
-
-	static final String dburl = "jdbc:mysql://localhost:3306/test_db"; // 순서 : 프토토콜(db종류에 따름) -> 나의위치 -> 포트번호 -> 스키마
+	static final String dburl = "jdbc:mysql://localhost:3306/test_db";
 	static final String dbuser = "test_user";
 	static final String dbpass = "test";
-
+	
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(dburl, dbuser, dbpass);
+			conn = DriverManager.getConnection(dburl,
+												dbuser, 
+												dbpass);
 		} catch (ClassNotFoundException e) {
 			System.err.println("드라이버 로드 실패!");
 		}
 		return conn;
 	}
-
+	
 	@Override
 	public List<AuthorVo> getList() {
 		List<AuthorVo> list = new ArrayList<>();
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-
+		
 		try {
 			conn = getConnection();
 			stmt = conn.createStatement();
-
-			String sql = "SELECT author_id, author_name, author_desc, regdate " + " FROM author";
+			
+			String sql = "SELECT author_id, author_name, author_desc, regdate " +
+					" FROM author";
 			rs = stmt.executeQuery(sql);
-
-			// 각 레코드를 List<AuthorVo>로 변환
+			
+			//	각 레코드를 List<AuthorVo>로 변환
 			while (rs.next()) {
 				Long authorId = rs.getLong(1);
 				String authorName = rs.getString(2);
 				String authorDesc = rs.getString(3);
 				Date regdate = rs.getDate(4);
-
+				
 				AuthorVo vo = new AuthorVo(authorId, authorName, authorDesc, regdate);
-
+				
 				list.add(vo);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 		return list;
 	}
 
@@ -76,16 +77,17 @@ public class AuthorDaoImpl implements AuthorDao {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		int insertedCount = 0;
-
+		
 		try {
 			conn = getConnection();
-
-			String sql = "INSERT INTO author (author_name, author_desc)" + " VALUES(?, ?)";
-
+			
+			String sql = "INSERT INTO author (author_name, author_desc) " +
+					" VALUES(?, ?)";
 			pstmt = conn.prepareStatement(sql);
+			
 			pstmt.setString(1, vo.getAuthorName());
-			pstmt.setString(1, vo.getAuthorDesc());
-
+			pstmt.setString(2, vo.getAuthorDesc());
+			
 			insertedCount = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -93,15 +95,12 @@ public class AuthorDaoImpl implements AuthorDao {
 			try {
 				if (pstmt != null) pstmt.close();
 				if (conn != null) conn.close();
-			} catch (Exception e) {
-
-			}
+			} catch (Exception e) {}
 		}
+		
 		return 1 == insertedCount;
-
 	}
 
-	
 	@Override
 	public boolean update(AuthorVo vo) {
 		// TODO Auto-generated method stub
@@ -110,8 +109,27 @@ public class AuthorDaoImpl implements AuthorDao {
 
 	@Override
 	public boolean delete(Long id) {
-		// TODO Auto-generated method stub
-		return false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int deletedCount = 0;
+		
+		try {
+			conn = getConnection();
+			String sql = "DELETE FROM author " +
+					" WHERE author_id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, id);
+			
+			deletedCount = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e) {}
+		}
+		return 1 == deletedCount;
 	}
 
 }
